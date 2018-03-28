21b9b465d1cbc3471f87ba8148cc2ae862d7d04c
Paul <pstolz2s@gmail.com>
2018-03-28 21:10:20 +0200

    test

---
+++ src/components/Block.vue
@@ -0,0 +1,127 @@
+<template>
+  <div tabindex="-1" :style="randomBackground">
+    <div class="hover">
+      hover
+    </div>
+    <div class="text">
+      {{ element }}
+    </div>
+    <div class="focus">
+      {{ element }}
+    </div>
+    <div class="overlay">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  props: ['element'],
+  data() {
+    return {
+      msg: 'ROAD',
+    };
+  },
+  computed: {
+    randomBackground() {
+      const hex = Math.floor(Math.random() * 0xFFFFFF);
+      console.log(this.element, hex);
+      return {
+        background: `#${hex.toString(16).substr(-6)}FF`,
+        color: '#FFFFFF',
+        'grid-area': this.element,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.block > * {
+  padding: 5px;
+
+}
+.text {
+  z-index: 10;
+}
+.block {
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  /* background-color: 1px solid rgba(0, 0, 0, 0.2); */
+  padding-top: 5px;
+  /* position: relative; */
+}
+.hover {
+  opacity: 0;
+  position: absolute;
+  transition: all 0.2s;
+  left: 0;
+}
+.focus {
+  opacity: 0;
+  position: absolute;
+  transition: all 0.2s;
+  right: 0;
+}
+.block:hover .hover{
+  opacity: 1;
+}
+.block:focus .focus{
+  opacity: 1;
+}
+.block:focus{
+  box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.5);
+}
+.browser {
+}
+.webApp {
+  grid-column-start: 10;
+  grid-column-end: 12;
+  grid-row-start: 5;
+}
+.webApi {
+  grid-column-start: 10;
+  grid-column-end: 12;
+  grid-row-start: 7;
+}
+.PSS {
+  grid-column-start: 10;
+  grid-column-end: 12;
+  grid-row-start: 9;
+}
+.psssearch {
+  grid-column-start: 10;
+  grid-column-end: 12;
+  grid-row-start: 11;
+}
+.redis1 {
+  grid-column-start: 8;
+  grid-column-end: 9;
+  grid-row-start: 5;
+}
+.redis2 {
+  grid-column-start: 8;
+  grid-column-end: 9;
+  grid-row-start: 7;
+}
+.cssearch {
+  grid-column-start: 14;
+  grid-column-end: 16;
+  grid-row-start: 11;
+}
+.COS {
+  grid-column-start: 14;
+  grid-column-end: 16;
+  grid-row-start: 8;
+}
+.CDA {
+  grid-column-start: 14;
+  grid-column-end: 16;
+  grid-row-start: 6;
+}
+.CMS {
+  grid-column-start: 16;
+  grid-column-end: 17;
+  grid-row-start: 8;
+}
+</style>
---
+++ src/components/StartPage.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="">
+    <h1>{{ msg }}</h1>
+    <div class="architecture">
+      <div is="Block" v-for="element in elements" :element="element" :class="`${element} block`">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Block from './Block';
+
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'ROAD',
+      elements: ['browser', 'webCache', 'webApp', 'webApi', 'redis1', 'redis2', 'PSS', 'psssearch', 'COS', 'cssearch', 'CDA'],
+    };
+  },
+  components: {
+    Block,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+.architecture {
+  display: grid;
+  /* grid-template-columns: repeat(20, 1fr);
+  grid-template-rows: 1fr 1fr 25px repeat(9, 1fr); */
+  position: relative;
+  background: url('../../static/img/bg.png');
+  background-size: 100% 100%;
+  width: 80vw;
+  height: 75vh;
+  margin: 0 10vw;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  grid-gap: 50px 100px;
+  grid-template-areas:
+    ".  .       browser   .   ."
+    ".  .       webCache  .   ."
+    ".  redis1  webApp    CDA ."
+    ".  redis2  webApi    .   ."
+    ".  .       PSS       COS ."
+    ".  .       psssearch cssearch .";
+}
+</style>
